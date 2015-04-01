fc1d7a908d4104b57a5fb14f328b43d8d69813a5
Runze Tang <tangrunze@gmail.com>
2015-03-31 21:32:06 -0400

    main for continuous case implemented.

--- main_conti.m
+++ main_conti.m
@@ -3,11 +3,10 @@ clear all
 close all
 
 %% --- Parameters Setting ---
-epsilon = 0.1;
-epsilonInB = 0.05;
-ratio = 2;
+epsilon = 0.2;
+epsilonInB = 0.01;
 
-m = 10;
+m = 20;
 nVertex = 150;
 nBlock = 3;
 theta = ones(1, nBlock);
@@ -16,20 +15,12 @@ rho = repmat(1/nBlock, 1, nBlock);
 iStart = 1;
 iEnd = 100;
 
-scale = 1;
-
+scaleB = 1;
+scaleVar = 10;
 
 % block probability matrix
 B = (0.5 - epsilonInB)*ones(nBlock, nBlock) + 2*epsilonInB*eye(nBlock);
-B = scale^2*B;
-
-% true nu (nBlock-by-dimLatentPosition)
-% nuStar = chol(B)';
-[U, S, ~] = svds(B, dimLatentPosition);
-nuStar = U*sqrt(S);
-
-% true spectral graph embedding Xhat (nBlock-by-dimLatentPosition)
-xHatHL = asge(B, dimLatentPosition);
+B = scaleB^2*B;
 
 %% --- Parallel Computing ---
 % delete(gcp('nocreate'))
@@ -38,15 +29,12 @@ xHatHL = asge(B, dimLatentPosition);
 
 %% Monte Carlo Simulation
 
-% errorRate = zeros(1, m);
-% errorRate1 = zeros(1, m);
-% errorRate2 = zeros(1, m);
-% for l = 5:m
-
 errorRateMean = zeros(1, iEnd);
 errorRateHL = zeros(1, iEnd);
 ADiffMean = zeros(1, iEnd);
 ADiffHL = zeros(1, iEnd);
+AXDiffMean = zeros(1, iEnd);
+AXDiffHL = zeros(1, iEnd);
 
 for iIter = iStart:iEnd
     adjMatrixTotal = zeros(m, nVertex, nVertex);
@@ -54,8 +42,8 @@ for iIter = iStart:iEnd
     for iGraph = ((iIter - 1)*m+1):(iIter*m)
         % Generate data if there does not exist one, otherwise read the
         % existing data.
-        [adjMatrix, tauStar, xStar, ~] = datagenerator_discrete(nVertex, nBlock, ...
-            dimLatentPosition, B, nuStar, rho, epsilon, scale, ratio, iGraph);
+        [adjMatrix, tauStar, ~] = datagenerator_conti(nVertex, ...
+            nBlock, B, rho, epsilon, scaleVar, iGraph);
         adjMatrixTotal(iGraph - (iIter - 1)*m, :, :) = adjMatrix;
         adjMatrixSum = adjMatrixSum + adjMatrix;
     end
@@ -63,32 +51,47 @@ for iIter = iStart:iEnd
     adjMatrixMean = adjMatrixSum/m;
     adjMatrixHL = hlcalculator(adjMatrixTotal, m);
     
+    PStar = B(tauStar, tauStar);
+    
     % mean
+    ADiffMean(iIter) = norm(adjMatrixMean - PStar);
+    
     xHatMean = asge(adjMatrixMean, dimLatentPosition);
     gm = fitgmdist(xHatMean, nBlock, 'Replicates', 10);
     tauHatMean = cluster(gm, xHatMean)';
     errorRateMean(iIter) = errorratecalculator(tauStar, tauHatMean, nBlock);
-    ADiffMean(iIter) = norm(xHatMean*xHatMean' - xStar*xStar');
+    
+    AXDiffMean(iIter) = norm(xHatMean*xHatMean' - PStar);
     
     % HL
+    ADiffHL(iIter) = norm(adjMatrixHL - PStar);
+    
     xHatHL = asge(adjMatrixHL, dimLatentPosition);
     gm = fitgmdist(xHatHL, nBlock, 'Replicates', 10);
     tauHatHL = cluster(gm, xHatHL)';
     errorRateHL(iIter) = errorratecalculator(tauStar, tauHatHL, nBlock);
-    ADiffHL(iIter) = norm(xHatHL*xHatHL' - xStar*xStar');
+    
+    AXDiffHL(iIter) = norm(xHatHL*xHatHL' - PStar);
 end
 
 [mean(errorRateMean), mean(errorRateHL)]
 
 % 1-sided sign-test HA errorRateMean > errorRateHL
 tmpStats = sum(errorRateMean > errorRateHL);
-pValue = 1 - binocdf(tmpStats - 1, iEnd, 0.5)
+pValueError = 1 - binocdf(tmpStats - 1, iEnd, 0.5)
 
 [mean(ADiffMean), mean(ADiffHL)]
 
 % 1-sided sign-test
 tmpStats = sum(ADiffMean > ADiffHL);
-pValue = 1 - binocdf(tmpStats - 1, iEnd, 0.5)
+pValueADiff = 1 - binocdf(tmpStats - 1, iEnd, 0.5)
+
+[mean(AXDiffMean), mean(AXDiffHL)]
+
+% 1-sided sign-test
+tmpStats = sum(AXDiffMean > AXDiffHL);
+pValueAXDiff = 1 - binocdf(tmpStats - 1, iEnd, 0.5)
+
 
 
 
