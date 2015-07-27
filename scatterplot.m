454bfaf9993f548d13ada60a2815c97650fb96ae
Runze Tang <tangrunze@gmail.com>
2015-07-27 10:05:53 -0400

    Function for drawing scatter plot.

--- scatterplot.m
+++ scatterplot.m
@@ -2,7 +2,8 @@ function scatterplot(xHat, tauHat, muHat, sigmaHat, color, comment)
 
     plot(xHat(:, 1), xHat(:, 2), strcat('.', color));
     hold on;
-    plot(muHat(:, 1), muHat(:, 2), strcat('x', color));
+    plot(muHat(:, 1), muHat(:, 2), strcat('s', color), 'MarkerEdgeColor', 'k',...
+                'MarkerFaceColor', color, 'MarkerSize', 5);
     
     ellipseplot(squeeze(muHat(1, :)), squeeze(sigmaHat(:, :, 1)), color,...
         'on');
