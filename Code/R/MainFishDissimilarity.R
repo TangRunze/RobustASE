723d801650c235363d68f6b67bf14413fe059af3
Runze Tang <tangrunze@gmail.com>
2016-12-18 13:25:46 -0500

    Dissimilarity result added.

--- Code/R/MainFishDissimilarity.R
+++ Code/R/MainFishDissimilarity.R
@@ -1,6 +1,6 @@
 rm(list = ls())
-# setwd("/Users/Runze/Documents/GitHub/RobustASE/Code/R")
-setwd("E:/GitHub/RobustASE/Code/R")
+setwd("/Users/Runze/Documents/GitHub/RobustASE/Code/R")
+# setwd("E:/GitHub/RobustASE/Code/R")
 
 # # load("../../Data/gg-v2-14_02_01-deltat50-th0.Rbin")
 # # load("../../Data/cov-v2-14_02_01-deltat50-th0.RBin")
